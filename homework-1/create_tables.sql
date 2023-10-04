c9dba1927a05a0619cc627dfa96172e2fa0b0af9
Oleg <panasenko23o@mail.ru>
2023-10-04 13:42:08 +1000

    create code for HW1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(20) NOT NULL,
+	last_name varchar(40) NOT NULL,
+	title varchar(50) NOT NULL,
+	birth_date date NOT NULL,
+	notes text
+);
+
+CREATE TABLE customers
+(
+	customer_id  varchar(50) PRIMARY KEY,
+	company_name varchar(60) NOT NULL,
+	contact_name varchar(30) NOT NULL
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(50) REFERENCES customers(customer_id),
+	employee_id int REFERENCES employees(employee_id),
+	order_date date NOT NULL,
+	ship_city varchar(20) NOT NULL
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,32 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+import csv
+import os
+import psycopg2
+
+conn = psycopg2.connect(host='localhost',
+                        port='5433', database='north',
+                        user='postgres',
+                        password=os.getenv('PG_ADMIN_PASS'))
+
+
+def intersection_to_db(file, query):
+    with open(file) as file:
+        reader = csv.reader(file)
+        next(reader)
+        with conn.cursor() as curs:
+            curs.executemany(query, reader)
+    conn.commit()
+
+
+query_customers = """INSERT INTO customers (customer_id,company_name,contact_name)
+                      VALUES (%s,%s,%s)"""
+query_employees = """INSERT INTO employees (employee_id,first_name,last_name,title,birth_date,notes)
+                      VALUES (%s,%s,%s,%s,%s,%s)"""
+query_orders = """INSERT INTO orders (order_id,customer_id,employee_id,order_date,ship_city)
+                      VALUES (%s,%s,%s,%s,%s)"""
+
+intersection_to_db('north_data/customers_data.csv', query_customers)
+intersection_to_db('north_data/employees_data.csv', query_employees)
+intersection_to_db('north_data/orders_data.csv', query_orders)
+conn.close()
